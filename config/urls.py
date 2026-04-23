35989bc8944ab73de32ca94785c1b9782864486f
M4x4T <annageldiyevmaksat94@gmail.com>
2026-04-23 23:25:36 +0300

    fix: add default English redirect and mobile hamburger menu

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.views.generic import RedirectView
 from apps.pages.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
+    path('', RedirectView.as_view(url='/en/', permanent=False)), 
 ]
 
 urlpatterns += i18n_patterns(
