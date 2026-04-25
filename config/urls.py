ce1737bc0c848bb608aa658103909507bece512d
M4x4T <annageldiyevmaksat94@gmail.com>
2026-04-25 21:12:49 +0300

    fix: language switcher now redirects to correct language

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
-from django.views.generic import RedirectView
+from django.utils.translation import get_language
+from django.shortcuts import redirect
 from apps.pages.views import home
+
+def root_redirect(request):
+    lang = get_language() or 'en'
+    return redirect(f'/{lang}/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
-    path('', RedirectView.as_view(url='/en/', permanent=False)), 
+    path('', root_redirect),  # ← reads the cookie and redirects correctly
 ]
 
 urlpatterns += i18n_patterns(
